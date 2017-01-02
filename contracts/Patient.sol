6f8116a8a4db11bd11f6db245808106f703bdc3b
Anant Joshi <anantjoshi1324@gmail.com>
2017-01-02 18:47:01 +0530

    Added variables for patient information

--- contracts/Patient.sol
+++ contracts/Patient.sol
@@ -3,6 +3,15 @@ import "./PatientRecord.sol";
 
 
 contract Patient{
-	address private patient;
-	PatientRecord private lastRecord;		
+	address private uid;//patient's ethereum address
+	PatientRecord private lastRecord;
+
+	//immutable patient information
+
+	//date (and preferably time) of birth
+	//in the UNIX format
+	int32 dateTimeOfBirth;
+
+	//sex of the patient, true if female, false if male
+	bool sex;
 }
